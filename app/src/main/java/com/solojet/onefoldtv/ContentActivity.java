4f5768aa753b46247c31db9820c6ff1a1e8184bb
Mendhie <imendhie@yahoo.com>
2020-09-14 19:07:44 +0100

    Modified video display algo

--- app/src/main/java/com/solojet/onefoldtv/ContentActivity.java
+++ app/src/main/java/com/solojet/onefoldtv/ContentActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -20,13 +21,18 @@ import com.google.firebase.firestore.Query;
 import adapters.ContentAdapter;
 
 import static config.Config.YOUTUBE_API_KEY;
+import static models.ConstantVariables.ALL;
+import static models.ConstantVariables.TYPE;
 
-public class ContentActivity extends YouTubeBaseActivity implements ContentAdapter.ItemClickListener {
+public class ContentActivity extends YouTubeBaseActivity implements ContentAdapter.ItemClickListener, YouTubePlayer.OnInitializedListener {
 
     private YouTubePlayerView youTubeView;
     private static final int RECOVERY_REQUEST = 1;
     private TextView txtDescription;
     private String index;
+    private YouTubePlayer player;
+    private boolean instantPlay;
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +43,21 @@ public class ContentActivity extends YouTubeBaseActivity implements ContentAdapt
         RecyclerView lstVideos = findViewById(R.id.lstVideos);
         txtDescription = findViewById(R.id.txtDes);
         lstVideos.setLayoutManager(new LinearLayoutManager(this));
+        youTubeView.initialize(YOUTUBE_API_KEY, this);
+
+        FloatingActionButton fab = findViewById(R.id.fabPost);
+        fab.setOnClickListener(view -> startActivity(new Intent(ContentActivity.this, UploadVideoActivity.class)));
+
+        //get video type
+        if(getIntent()!=null)
+            type = getIntent().getStringExtra(TYPE);
 
         CollectionReference ref = FirebaseFirestore.getInstance().collection("videos");
-        Query query = ref.orderBy("time", Query.Direction.DESCENDING);
+        Query query;
+        if(type==null || type.isEmpty() || type.equals(ALL))
+            query = ref.orderBy("time", Query.Direction.DESCENDING);
+        else
+            query = ref.orderBy("time", Query.Direction.DESCENDING).whereEqualTo(TYPE, type);
 
         ContentAdapter adapter = new ContentAdapter(this, query);
         lstVideos.setAdapter(adapter);
@@ -53,45 +71,68 @@ public class ContentActivity extends YouTubeBaseActivity implements ContentAdapt
             txtDescription.setVisibility(View.VISIBLE);
     }
 
-    private void playVideo(){
-        youTubeView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRstored) {
-                if(!wasRstored)
-                    youTubePlayer.cueVideo(index);
-            }
-
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult initializationResult) {
-                if (initializationResult.isUserRecoverableError()) {
-                    initializationResult.getErrorDialog(ContentActivity.this, RECOVERY_REQUEST).show();
-                } else {
-                    String error = String.format(getString(R.string.player_error), initializationResult.toString());
-                    Toast.makeText(ContentActivity.this, error, Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
-
     @Override
-    public void onItemClicked(String index) {
-        setIndex(index);
-        playVideo();
+    public void onItemClicked(String index, boolean instantPlay) {
+        this.index = index;
+        this.instantPlay = instantPlay;
+        if(player!=null)
+            player.cueVideo(index);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RECOVERY_REQUEST) {
             // Retry initialization if user performed a recovery action
-            playVideo();
+            youTubeView.initialize(YOUTUBE_API_KEY, this);
         }
     }
 
-    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
-        return youTubeView;
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        player = youTubePlayer;
+        player.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+            @Override
+            public void onLoading() {
+
+            }
+
+            @Override
+            public void onLoaded(String s) {
+                if(instantPlay)
+                    player.play();
+            }
+
+            @Override
+            public void onAdStarted() {
+
+            }
+
+            @Override
+            public void onVideoStarted() {
+
+            }
+
+            @Override
+            public void onVideoEnded() {
+
+            }
+
+            @Override
+            public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+            }
+        });
+        if(index!=null)
+            player.cueVideo(index);
     }
 
-    public void setIndex(String index) {
-        this.index = index;
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult initializationResult) {
+        if (initializationResult.isUserRecoverableError()) {
+            initializationResult.getErrorDialog(ContentActivity.this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), initializationResult.toString());
+            Toast.makeText(ContentActivity.this, error, Toast.LENGTH_LONG).show();
+        }
     }
 }
